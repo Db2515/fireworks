4ef469112c4b4de0c242e4ae6e4748d53f1d718c
Daniel Bailey <daniel.bailey@arm.com>
2021-03-12 18:38:30 +0000

    No flicker, no cursor, no flicker, also removed cursor and fixed flicker
    (this commit message brought to you by the "character limits will be
    enforced" gang)
    
    Real commit message:
    Avoid clearing the screen fully between frames, and avoid redrawing the
    text, to minimise flicker. Also hide the cursor

--- fireworks.sh
+++ fireworks.sh
@@ -5,13 +5,27 @@ pattern_files_extension=".txt"
 patterns_dir="patterns"
 nb_frames=58
 
+# when interrupted, reenable cursor and skip over the text below the cursor
+int() {
+  tput cnorm
+  echo -e '\n\n\n\n\n\n\n\n\n\n'
+  exit 0
+}
+trap int SIGINT
+
+# make cursor invisible
+tput civis
+
+clear
+# put the first frame and bottom text on the screen
+cat ${patterns_dir}/1${pattern_files_extension} ${patterns_dir}/base${pattern_files_extension}
 while (true)
 do
   for ((i = 1; i <= ${nb_frames} ; i += 1))
   do
+    # go to coords (0,0), then show the next frame
+    tput cup 0 0
     cat ${patterns_dir}/${i}${pattern_files_extension}
-    cat ${patterns_dir}/base${pattern_files_extension}
     sleep 0.04
-    clear
   done
 done
