f80bad84322d6d65544030d05ecb8f5b1cf62a37
Db2515 <daniel@boulby.plus.com>
2021-03-12 10:21:43 +0000

    Create infinite loop for animation

--- fireworks.sh
+++ fireworks.sh
@@ -5,9 +5,13 @@ pattern_files_extension=".txt"
 patterns_dir="patterns"
 nb_frames=58
 
-for ((i = 0; i <= ${nb_frames} ; i += 1))
+while (true)
 do
+  for ((i = 1; i <= ${nb_frames} ; i += 1))
+  do
     cat ${patterns_dir}/${i}${pattern_files_extension}
+    cat ${patterns_dir}/base${pattern_files_extension}
     sleep 0.04
     clear
+  done
 done
